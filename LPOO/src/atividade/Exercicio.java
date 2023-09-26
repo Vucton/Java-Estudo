6466ad0cd9aec9ada6e122c0368bdc3130e0e428
Vucton <Vucton@DESKTOP-MPS4DF1.mshome.net>
2023-09-25 23:43:32 -0300

    Resolvendo alguns exercicios basicos com a linguagem

--- LPOO/src/atividade/Exercicio.java
+++ LPOO/src/atividade/Exercicio.java
@@ -3,8 +3,9 @@ package atividade;
 public class Exercicio {
 
 	public static void main(String[] args) {
-		System.out.println("Primeiro programa");
-		System.out.println("Nome do programador: João Victor Rodrigues");
+		System.out.println("Primeiro programa em Java");
+		System.out.println("Nome do programador: Joao Victor Rodrigues");
+		
 		
 		
 	}
